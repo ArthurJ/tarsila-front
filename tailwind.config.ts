631b988c3a1fa9208e76ced39c950c3d4b3bad6c
Arthur Julião <ArthurJ@users.noreply.github.com>
2025-10-21 21:04:36 -0300

    Initialize Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  //plugins: [
+  //  require('@tailwindcss/typography'),
+  //],
+} satisfies Config;
